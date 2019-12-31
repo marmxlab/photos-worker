c9b20e84066bed167648d9c6b0a03a182c46fa2f
Martin Yeung <myeung@finfabrik.com>
2019-12-31 21:20:22 +0800

    make redis password configurable

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ const queue = new Bull('thumbnail-generations', {
   redis: {
     port: process.env.REDIS_PORT || 6379,
     host: process.env.REDIS_HOST || 'localhost',
+    password: process.env.REDIS_PASSWORD || undefined,
   }
 });
 
