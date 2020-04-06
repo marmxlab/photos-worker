702e01d60e40850d8f8fde62dd02a36990fb9660
Martin Yeung <marmx.lab@gmail.com>
2020-04-06 23:11:31 +0800

    Support mov files

--- utils/file.ts
+++ utils/file.ts
@@ -1,5 +1,5 @@
 const IMAGE_MIMES = ['image/jpeg', 'image/bmp', 'image/png', 'image/webp', 'image/heic'];
-const VIDEO_MIMES = ['video/mp4', 'video/mpeg', 'video/ogg', 'video/webm'];
+const VIDEO_MIMES = ['video/mp4', 'video/mpeg', 'video/ogg', 'video/webm', 'video/quicktime'];
 
 export default class FileUtil {
   public static isSupportedImageMIME(mime: string): boolean {
